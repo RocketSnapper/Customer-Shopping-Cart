46a6a636a602feeebd594ff58b6ca4d54005e232
usjamesmc <usjamesmc@gmail.com>
2025-11-14 20:26:03 -0600

    Updated shopping cart methods

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ from product import Product
 from shoppingcart import ShoppingCart
 
 product_one = Product('Blender', 49.99, 'Kitchen appliances')
-product_one.print_product()
-
 product_two = Product('Baseball Bat', 65.98, 'Sports')
 
-product_three = Product('Tent', 103.67, 'Outdoors')
-
-
-shoppingcart_one = ShoppingCart([product_one.name, product_one.price, product_one.category], {product_two})
-shoppingcart_one.price_cart(product_one.price, product_two.price, product_three.price)
-shoppingcart_one.add_product()
+shoppingcart_one = ShoppingCart([product_one.name, product_one.price, product_one.category], [product_two.name, product_two.price, product_two.category])
+shoppingcart_one.add_product(['Necklace', 75.99, 'Jewelry'])
+cart_price = shoppingcart_one.price_cart()
+shoppingcart_one.empty_cart()
 
--- shoppingcart.py
+++ shoppingcart.py
@@ -2,13 +2,16 @@ class ShoppingCart:
     def __init__(self, product_one, product_two):
         self.products = product_one + product_two
 
-    def add_product(self):
+    def add_product(self, new_product):
+        self.products += new_product
         print(self.products)
     
-    def price_cart(self,product_a, product_b, product_c):
-        cart_total = product_a + product_b +product_c
-        print(cart_total)
+    def price_cart(self):
+        cart_total = 0.00
+        for product in self.products:
+            if type(product) == float:
+                cart_total += product
         return cart_total
 
-    def empty_cart():
-        print()
+    def empty_cart(self):
+        self.products = []
